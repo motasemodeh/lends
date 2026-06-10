82108d95dea71b29b5133bce34ccb50464c9cf28
motasemodeh <motasem.udeh@gmail.com>
2026-06-11 00:35:09 +0300

    Fix wrapper script to use corepack explicitly

--- .yarn/yarn-wrapper.js
+++ .yarn/yarn-wrapper.js
@@ -7,7 +7,7 @@ const args = process.argv.slice(2).filter(arg => !arg.startsWith('--production')
 // We set YARN_IGNORE_PATH=1 so that the real yarn doesn't call this wrapper again.
 const env = { ...process.env, YARN_IGNORE_PATH: '1' };
 
-const child = cp.spawn(process.platform === 'win32' ? 'yarn.cmd' : 'yarn', args, { stdio: 'inherit', env, shell: process.platform === 'win32' });
+const child = cp.spawn(process.platform === 'win32' ? 'corepack.cmd' : 'corepack', ['yarn', ...args], { stdio: 'inherit', env, shell: process.platform === 'win32' });
 
 child.on('exit', code => process.exit(code));
 child.on('error', err => {
