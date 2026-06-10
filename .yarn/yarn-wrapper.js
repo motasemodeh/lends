feb95049dd1c026a946172ea2d736447423b4b9f
motasemodeh <motasem.udeh@gmail.com>
2026-06-11 00:39:11 +0300

    Strip all legacy flags from Heroku Yarn 1 fallback

--- .yarn/yarn-wrapper.js
+++ .yarn/yarn-wrapper.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 const cp = require('child_process');
 
-// Remove --production=true or --production=false from arguments
-const args = process.argv.slice(2).filter(arg => !arg.startsWith('--production'));
+// Remove Heroku's legacy flags that crash Yarn 4
+const invalidFlags = ['--production', '--ignore-engines', '--prefer-offline'];
+let args = process.argv.slice(2).filter(arg => !invalidFlags.some(f => arg.startsWith(f)));
+
+// Map frozen-lockfile to immutable
+args = args.map(arg => arg === '--frozen-lockfile' ? '--immutable' : arg);
 
 // We set YARN_IGNORE_PATH=1 so that the real yarn doesn't call this wrapper again.
 const env = { ...process.env, YARN_IGNORE_PATH: '1' };
