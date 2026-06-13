3ad01b77f6707e4d33a13f12f592cd13ae75b5e4
motasemodeh <motasem.udeh@gmail.com>
2026-06-13 17:36:19 +0300

    Dynamically inject Contact Us link into footer alongside TOS and Privacy Policy

--- src/containers/FooterContainer/FooterContainer.js
+++ src/containers/FooterContainer/FooterContainer.js
@@ -17,11 +17,49 @@ const FooterComponent = () => {
     return null;
   }
 
+  const blocks = footer.blocks ? [...footer.blocks] : [];
+  
+  // Try to find the block containing the "Privacy" or "Terms" link
+  const targetBlockIndex = blocks.findIndex(block => 
+    block.links && block.links.some(link => link.href && (link.href.includes('terms-of-service') || link.href.includes('privacy-policy')))
+  );
+
+  if (targetBlockIndex > -1) {
+    const block = { ...blocks[targetBlockIndex] };
+    const links = [...(block.links || [])];
+    
+    const hasContactUs = links.some(link => link.href && link.href.includes('/support'));
+    if (!hasContactUs) {
+      links.push({
+        fieldType: 'link',
+        content: 'Contact Us',
+        href: '/support'
+      });
+      block.links = links;
+      blocks[targetBlockIndex] = block;
+    }
+  } else if (blocks.length > 0) {
+    // If we can't find it, just add it to the first column
+    const block = { ...blocks[0] };
+    const links = [...(block.links || [])];
+    const hasContactUs = links.some(link => link.href && link.href.includes('/support'));
+    if (!hasContactUs) {
+      links.push({
+        fieldType: 'link',
+        content: 'Contact Us',
+        href: '/support'
+      });
+      block.links = links;
+      blocks[0] = block;
+    }
+  }
+
   // The footer asset does not specify sectionId or sectionType. However, the SectionBuilder
   // expects sectionId and sectionType in order to identify the section. We add those
   // attributes here before passing the asset to SectionBuilder.
   const footerSection = {
     ...footer,
+    blocks,
     sectionId: 'footer',
     sectionType: 'footer',
     linkLogoToExternalSite: topbar?.logoLink,
